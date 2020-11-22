5d45fe245c62f77f535b0230028043f3d1cc167b
iampham <ampham@g.hmc.edu>
2020-11-21 20:55:54 -0500

    Finished crystla plasticity first draft

--- calculatePlastic.py
+++ calculatePlastic.py
@@ -1,4 +1,4 @@
-def calculatePlastic(gamma_dot_ref, slip_exp, q_si, q_sat, h_0, q_sisj,  F_p_prev):
+def calculatePlastic(gamma_dot_ref, m, g, g_sat, g_prev, a, h, dt, F_e):
     """ 
     Calculates the plastic part of the deformation tensor. Uses the following equation:
     F_p = deltaGamma dot F_p_prev
@@ -7,19 +7,57 @@ def calculatePlastic(gamma_dot_ref, slip_exp, q_si, q_sat, h_0, q_sisj,  F_p_pre
 
     INPUTS: 
     gamma_dot: reference slip rate
-    slip_exp: slip rate exponent
+    m: slip rate exponent
 
-    g_si 
+    g - resistance vector
     g_sat
     h_0
     q_sisj
 
-
+    
     """ 
 
+    F = np.zeros([3,3])
+    F[0:1,0:1] = F_e
+    F[2,2] = 1
 
+    strainIncrement = np.zeros([3,3])
+
+    slipRates = np.zeros([10,1])
+
+    for index in range(10):
+
+        slipRates[index] = calculateSlipRate(gamma_dot_ref, m, g[index], index)
+        strainIncrement += slipRates[index] * schmidTensor
     
-    return F_p
+    g_next = getNextResistance(g_sat, g, a, h, slipRates, dt)
+
+    return strainIncrement[0:2, 0:2], g_next
+
+def calculateSlipRate(gamma_dot_ref, m, g_si, index):
+
+    schmidTensor = getSchmidTensor(index)
+    tau_s = np.dot(np.matmul(F.transpose, np.matmul(F,S)), schmidTensor) 
+
+    tau_th_s = getStrengthRatio(index) * g_si
+    slipRate = gamma_dot_ref * sign(tau_s) * np.abs(tau_s/tau_th_s)**(1/m)
+
+    return slipRate
+
+def getNextResistance(g_sat, g_prev, a, h, slipRates, dt):
+
+    g_dot_si = np.zeros([10,1])
+
+
+    for i in range(10):
+g_si
+        g_dot_si[i] = h * (1-g_prev[i]/g_sat)**a * slipRates[i]
+
+
+    g_current = g_prev + dt*g_dot_si
+    return g_current
+
+
 
 
 def getSchmidTensor(index):
@@ -29,6 +67,24 @@ def getSchmidTensor(index):
     schmidTensor = np.tensordot(slipDirection, slipPlane, axes = 0) # axes = 0 means tensor product
 
     return schmidTensor
+
+def getStrengthRatio(index):
+
+    strengthRatios = np.array([1,\
+    0.963,\
+    0.963,\
+    0.933,\
+    1.681,\
+    0.376,\
+    0.931,\
+    0.931,\
+    0.701,\
+    0.701])
+
+    strengthRatio = strengthRatios[i]
+
+    return strengthRatio
+
 
 def getSlipSystems(index):
 
