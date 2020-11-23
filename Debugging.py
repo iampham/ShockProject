4a2b05b30061221993b37c315a4439a11dc06827
DEMG397 <74153667+DEMG397@users.noreply.github.com>
2020-11-22 19:26:35 -0500

    elastic tensor C

--- Debugging.py
+++ Debugging.py
@@ -52,7 +52,13 @@ g_sat = 155.73e6 # Saturation slip resistance [Pa]
 g_prev = 1*np.ones([10,1]) # NEED CORRECT VALUE [Pa]
 a = 2.5 # Hardening exponent []
 h = 9.34e6 # Hardening matrix [Pa]
-
+# Elastic Tensor CC
+C_ela = np.array([[21.15,10.18,9.77,0.058,4.05,-0.18],
+                  [10.18,20.34,13.35,0.23,6.96,0.14],
+                  [9.77,13.35,21.27,-0.004,5.01,0.19],
+                  [0.058,0.23,-0.004,8.79,0.32,4.16],
+                  [4.05,6.96,5.01,0.32,6.20,0.22],
+                  [-0.18,0.14,0.19,4.16,0.22,10.00]])
 
 
 const_dictionary={"Gamma" : 0.7, # Mie Gruneisen Parameter []
